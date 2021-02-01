7271ac27a31da84a011f8483dee0a74317cdaca0
Leo.Zhang <Leo.Zhang2@sbdinc.com>
2021-02-01 17:35:37 +0800

    change home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Azure DevOps Testing" />
+    <HelloWorld msg="Change Home Page" />
   </div>
 </template>
 
